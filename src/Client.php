a2c03712ab063a1398cc647067fd00698e31ebd4
Arshid <arshidkv12@gmail.com>
2025-10-24 18:17:10 +0530

    v1.1.0

--- src/Client.php
+++ src/Client.php
@@ -40,7 +40,21 @@ class Client
     private function _send(string $cmd): string
     {
         $this->conn->send($cmd . "\n");
-        return trim($this->conn->recv());
+        $resp = trim($this->conn->recv());
+
+        if (strlen($resp) > 0 && $resp[0] === '$') {
+            // read length
+            $len = (int)substr($resp, 1);
+            if ($len === -1) {
+                return 'NONE';  
+            }
+
+            // Read actual data
+            $data = trim($this->conn->recv($len + 2)); // +2 for CRLF
+            return $data;
+        }
+
+        return $resp;
     }
 
     public function auth(string $password): bool
